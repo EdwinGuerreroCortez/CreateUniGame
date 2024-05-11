d31038a6bd3c6232b41a3092a10b24b44b7521bf
GeradoDomingo <20221110@uthh.edu.mx>
2024-05-10 19:44:25 -0600

    add:game

--- src/componentes/public/paginaPrincipal.jsx
+++ src/componentes/public/paginaPrincipal.jsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import Slider from 'react-slick';
 import juego2 from '../img/juego2.png';
@@ -8,6 +7,8 @@ import juego5 from '../img/juego5.jpg';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import '../CSS/style.css'; // Importa el archivo CSS de Bulma aquí
+
+
 const PaginaPrincipal = () => {
   const settings = {
     dots: true,
@@ -21,41 +22,55 @@ const PaginaPrincipal = () => {
       return <div className="dot"></div>; // Personaliza el estilo de los puntos de navegación
     }
   };
-  
+
   return (
     <section className="section has-background-black-bis">
-      <div className="container" style={{ position: 'relative', minHeight: '100vh' }}>
-        <div className="slider-container" style={{ position: 'absolute', top: '0', left: '0', bottom: '0', width: '50%' }}>
-          <Slider {...settings}>
-            <div className="slider-slide">
-              <img src={juego2} alt="Slide 2" style={{ width: '100%', maxHeight: '100vh', objectFit: 'cover' }} />
-            </div>
-            <div className="slider-slide">
-              <img src={juego3} alt="Slide 3" style={{ width: '100%', maxHeight: '100vh', objectFit: 'cover' }} />
-            </div>
-            <div className="slider-slide">
-              <img src={juego4} alt="Slide 4" style={{ width: '100%', maxHeight: '100vh', objectFit: 'cover' }} />
-            </div>
-            <div className="slider-slide">
-              <img src={juego5} alt="Slide 5" style={{ width: '100%', maxHeight: '100vh', objectFit: 'cover' }} />
-            </div>
-          </Slider>
-        </div>
-        <div className="card tarjeta-larga has-background-black has-border-white has-shadow" style={{ position: 'absolute', top: '0', right: '0', bottom: '0', width: '45%', overflowY: 'auto',height:'53%' }}>
-          <div className="card-content">
-            <h1 className="title has-text-centered has-text-white">Bienvenido a nuestra plataforma</h1>
-            <p className="subtitle has-text-centered has-text-white">
-              Aprende a crear tu propio juego con Unity y C#
-            </p>
-            <div className="content has-text-white">
-              <p>
-              Aprenderás a crear tus juegos con Unity y C#. Aquí encontrarás una variedad de videos tutoriales que te guiarán paso a paso a través del proceso de creación de tu propio juego, desde los conceptos básicos hasta técnicas avanzadas.
-              </p>
-              <p>
-                
-              </p>
+      <div className="container">
+        <div className="columns is-vcentered">
+          <div className="column is-half">
+            <Slider {...settings}>
+              <div className="slider-slide">
+                <img src={juego2} alt="Slide 2" style={{ width: '100%', maxHeight: '100vh', objectFit: 'cover' }} />
+              </div>
+              <div className="slider-slide">
+                <img src={juego3} alt="Slide 3" style={{ width: '100%', maxHeight: '100vh', objectFit: 'cover' }} />
+              </div>
+              <div className="slider-slide">
+                <img src={juego4} alt="Slide 4" style={{ width: '100%', maxHeight: '100vh', objectFit: 'cover' }} />
+              </div>
+              <div className="slider-slide">
+                <img src={juego5} alt="Slide 5" style={{ width: '100%', maxHeight: '100vh', objectFit: 'cover' }} />
+              </div>
+            </Slider>
+          </div>
+          <div className="column is-half">
+            <div className="card tarjeta-larga has-background-black has-border-white has-shadow">
+              <div className="card-content">
+                <h1 className="title has-text-centered has-text-white">Bienvenido a nuestra plataforma</h1>
+                <p className="subtitle has-text-centered has-text-white">
+                  Aprende a crear tu propio juego con Unity y C#
+                </p>
+                <div className="content has-text-white">
+                  <p>
+                    Aprenderás a crear tus juegos con Unity y C#. Aquí encontrarás una variedad de videos tutoriales que te guiarán paso a paso a través del proceso de creación de tu propio juego, desde los conceptos básicos hasta técnicas avanzadas.
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
+        </div>
+        <div className="has-text-white has-text-centered" style={{ marginTop: '20px' }}>
+          <h1 className="is-size-3">
+            Comienza usar Unity. Da vida a todo lo que puedas imaginar con Unity.
+          </h1>
+          <div className="container">
+  <h1 className="is-size-3 animate__animated animate__bounceInLeft color-change">
+    ¡Empieza tu trayecto de aprendizaje hoy!
+  </h1>
+</div>
+
+
+
         </div>
       </div>
     </section>
---
+++ src/componentes/usuario/bienvenida.jsx
@@ -0,0 +1,36 @@
+// Bienvenida.jsx
+import React from 'react';
+import unityLogo from  '../img/unitylogo.jpg';
+import cSharpLogo from  '../img/csharplogo.png';
+
+const Bienvenida = ({ usuario }) => {
+  return (
+    <div className="section">
+      <div className="container">
+        <h1 className="title">Bienvenido, {usuario.nombre}!</h1>
+        <p>Gracias por unirte a nuestra plataforma.</p>
+        <p>En esta plataforma encontrarás una gran variedad de recursos y contenido para aprender y mejorar tus habilidades en el desarrollo de videojuegos con Unity y C#.</p>
+        
+        <h2 className="subtitle">Tecnologías utilizadas:</h2>
+        <div className="columns">
+          <div className="column">
+            <div className="content has-text-centered">
+              <img src={unityLogo} alt="Logo de Unity" style={{ maxWidth: '150px' }} />
+              <p>Descarga Unity</p>
+              <a className="button is-primary is-rounded" href="https://unity.com/es" target="_blank" rel="noopener noreferrer">Descargar</a>
+            </div>
+          </div>
+          <div className="column">
+            <div className="content has-text-centered">
+              <img src={cSharpLogo} alt="Logo de C#" style={{ maxWidth: '150px' }} />
+              <p>Descarga Visual Studio con C#</p>
+              <a className="button is-primary is-rounded" href="https://visualstudio.microsoft.com/es/downloads/" target="_blank" rel="noopener noreferrer">Descargar</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Bienvenida;
