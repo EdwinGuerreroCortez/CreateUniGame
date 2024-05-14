af2e074447cab2e560f2e60279215cbce31d900d
GerardoDomingo <20221110@uthh.edu.mx>
2024-05-14 12:11:40 -0600

    add:game

--- src/componentes/usuario/bienvenida.jsx
+++ src/componentes/usuario/bienvenida.jsx
@@ -6,7 +6,7 @@ const Bienvenida = () => {
     <div className="section has-background-black-bis">
       <div className="container">
         <div className="columns is-centered">
-          <div className="column is-half-tablet is-one-third-desktop"> {/* Ajuste de columna para diferentes pantallas */}
+          <div className="column is-half-tablet is-one-third-desktop" style={{ marginBottom: '1.5rem', marginRight: '1rem' }}> {/* Ajuste de columna para diferentes pantallas y margen */}
             <div className="card profile-card" style={{ backgroundColor: '#001f3f', border: '2px solid #3273dc', borderRadius: '8px', boxShadow: '0 4px 8px rgba(50, 115, 220, 0.5)', color: '#ffffff', padding: '1.1rem', height: '100%' }}>
               <div className="card-content has-text-centered">
                 <div className="content">
@@ -21,7 +21,7 @@ const Bienvenida = () => {
               </div>
             </div>
           </div>
-          <div className="column is-half-tablet is-one-third-desktop"> {/* Ajuste de columna para diferentes pantallas */}
+          <div className="column is-half-tablet is-one-third-desktop" style={{ marginBottom: '1.5rem', marginLeft: '1rem' }}> {/* Ajuste de columna para diferentes pantallas y margen */}
             <div className="card" style={{ backgroundColor: '#001f3f', border: '2px solid #3273dc', borderRadius: '8px', boxShadow: '0 4px 8px rgba(50, 115, 220, 0.5)', color: '#ffffff', height: '100%' }}>
               <div className="card-content has-text-centered">
                 <div className="content">
