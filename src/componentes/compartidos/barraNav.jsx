6c2f5b01ffc408ca827df20ce53f5063033e579e
GerardoDomingo <20221110@uthh.edu.mx>
2024-05-24 16:10:48 -0600

    add:game

--- src/componentes/compartidos/barraNav.jsx
+++ src/componentes/compartidos/barraNav.jsx
@@ -22,7 +22,7 @@ const BarraNav = () => {
       <div className="navbar-brand">
         <img src={logo} alt="Logo" className="navbar-item" style={{ height: '5rem' }} />
         <Link className="navbar-item has-text-white animated-letters" to="/">
-          {renderAnimatedLetters("UniGame")}
+          {renderAnimatedLetters("MasterGame")}
         </Link>
         <a role="button" className={`navbar-burger burger ${isActive ? 'is-active' : ''}`} aria-label="menu" aria-expanded={isActive ? 'true' : 'false'} onClick={toggleBurgerMenu}>
           <span aria-hidden="true"></span>
