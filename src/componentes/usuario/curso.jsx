c41283895fd32aadb14bb905ba479985914d0ab3
EmmanuelRM17 <emma041117@gmail.com>
2024-05-25 21:44:32 -0600

    add:game

--- src/componentes/usuario/curso.jsx
+++ src/componentes/usuario/curso.jsx
@@ -222,29 +222,28 @@ const Curso = () => {
                 </form>
               </div>
             ) : (
-              <div className="box has-text-white" style={{ background: 'rgb(2, 25, 41)' }}>
-                <h2 className="title is-4 has-text-white">
-                  Seleccione un tema para ver los detalles
-                </h2>
-                <div aria-label="Orange and tan hamster running in a metal wheel" role="img" className="wheel-and-hamster">
-                  <div className="wheel"></div>
-                  <div className="hamster">
-                    <div className="hamster__body">
-                      <div className="hamster__head">
-                        <div className="hamster__ear"></div>
-                        <div className="hamster__eye"></div>
-                        <div className="hamster__nose"></div>
-                      </div>
-                      <div className="hamster__limb hamster__limb--fr"></div>
-                      <div className="hamster__limb hamster__limb--fl"></div>
-                      <div className="hamster__limb hamster__limb--br"></div>
-                      <div className="hamster__limb hamster__limb--bl"></div>
-                      <div className="hamster__tail"></div>
+              <div className="box has-text-white" style={{ background: 'rgb(2, 25, 41)', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
+              <h2 className="title is-4 has-text-white" style={{ textAlign: 'center' }}>
+              Por favor, elige un tema para ver más información              </h2>
+              <div aria-label="Orange and tan hamster running in a metal wheel" role="img" className="wheel-and-hamster" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <div className="wheel"></div>
+                <div className="hamster">
+                  <div className="hamster__body">
+                    <div className="hamster__head">
+                      <div className="hamster__ear"></div>
+                      <div className="hamster__eye"></div>
+                      <div className="hamster__nose"></div>
                     </div>
+                    <div className="hamster__limb hamster__limb--fr"></div>
+                    <div className="hamster__limb hamster__limb--fl"></div>
+                    <div className="hamster__limb hamster__limb--br"></div>
+                    <div className="hamster__limb hamster__limb--bl"></div>
+                    <div className="hamster__tail"></div>
                   </div>
-                  <div className="spoke"></div>
                 </div>
+                <div className="spoke"></div>
               </div>
+            </div>
             )}
           </div>
         </div>
