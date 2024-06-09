e86504a3fa2131c3e5aa0fdbcf926a9e9194b4ca
GerardoDomingo <20221110@uthh.edu.mx>
2024-06-08 19:07:32 -0600

    add:game

--- src/componentes/public/contactanos.jsx
+++ src/componentes/public/contactanos.jsx
@@ -1,9 +1,8 @@
-// src/componentes/public/ContactForm.js
-
 import React, { useState } from 'react';
-import contactImage from '../img/contactanos.webp'; // Suponiendo que tienes una imagen representativa
 import 'bulma/css/bulma.min.css';
 import '../CSS/ContactForm.css'; // Archivo CSS para los estilos adicionales
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const ContactForm = () => {
   const [formData, setFormData] = useState({
@@ -11,6 +10,9 @@ const ContactForm = () => {
     message: '',
     type: 'sugerencia' // valor predeterminado
   });
+
+  const [messages, setMessages] = useState([]);
+  const username = 'Gerardo Domingo';
 
   const handleChange = (e) => {
     setFormData({
@@ -21,21 +23,29 @@ const ContactForm = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Aquí puedes manejar el envío del formulario, por ejemplo, enviando los datos a una API
-    console.log('Form data submitted:', formData);
+    const newMessage = {
+      type: formData.type.toUpperCase(),
+      text: formData.message,
+      email: formData.email
+    };
+    setMessages([...messages, { username, ...newMessage }]);
+    setFormData({
+      email: '',
+      message: '',
+      type: 'sugerencia'
+    });
+  };
+
+  const handleDeleteMessage = (index) => {
+    setMessages(messages.filter((_, i) => i !== index));
   };
 
   return (
     <section className="section" style={styles.section}>
       <div className="container">
-        <div className="columns is-vcentered is-variable is-8">
+        <div className="columns is-variable is-8">
           <div className="column is-half">
-            <figure className="image-container" style={styles.imageContainer}>
-              <img src={contactImage} alt="Contact Us" style={styles.image} />
-            </figure>
-          </div>
-          <div className="column is-half">
-            <div className="box has-background-dark" style={styles.box}>
+            <div className="box has-background-dark">
               <h1 className="title has-text-white is-size-3">Contáctanos</h1>
               <p className="has-text-white">
                 Para cualquier sugerencia o queja, por favor completa el siguiente formulario.
@@ -56,45 +66,69 @@ const ContactForm = () => {
                   <label className="label has-text-white">Correo Electrónico</label>
                   <div className="control has-icons-left">
                     <input 
-                      className="input has-icons-left" 
+                      className="input has-background-dark has-text-white" 
                       type="email" 
                       name="email" 
                       value={formData.email} 
                       onChange={handleChange} 
                       required 
                       placeholder="e.g. alex@example.com"
-                      style={styles.input} 
+                      style={styles.input}
                     />
                     <span className="icon is-small is-left">
-                      <i className="fas fa-envelope"></i>
+                      <FontAwesomeIcon icon={faEnvelope} />
                     </span>
                   </div>
                 </div>
                 <div className="field">
                   <label className="label has-text-white">Mensaje</label>
-                  <div className="control has-icons-left">
+                  <div className="control">
                     <textarea 
-                      className="textarea has-icons-left" 
+                      className="textarea has-background-dark has-text-white" 
                       name="message" 
                       value={formData.message} 
                       onChange={handleChange} 
                       required 
                       placeholder="Escribe tu mensaje aquí"
-                      style={styles.textarea} 
+                      style={styles.textarea}
                     />
-                    <span className="icon is-small is-left">
-                      <i className="fas fa-comment-dots"></i>
-                    </span>
                   </div>
                 </div>
                 <div className="field">
                   <div className="control">
-                    <button className="button is-primary" type="submit" style={styles.button}>
+                    <button className="button is-primary" type="submit" style={styles.btnenviar}>
                       Enviar
                     </button>
                   </div>
                 </div>
               </form>
+            </div>
+          </div>
+          <div className="column is-half">
+            <div className="box has-background-dark">
+              <div className="has-text-centered">
+                <FontAwesomeIcon icon={faEnvelope} size="2x" color="white" />
+              </div>
+              <div className="messages" style={styles.messages}>
+              {messages.map((message, index) => (
+             <div key={index} className="message-box">
+             <p className="has-text-info">{message.username}</p>
+            <div className="box has-background-dark">
+           <p className="has-text-white">{message.type}</p>
+           <div className="message-content">
+           <p className="has-text-white message-text">{message.text}</p> {/* Aplica la clase message-text aquí */}
+           </div>
+      <p className="has-text-grey-light">{message.email}</p>
+    </div>
+    <button 
+      className="button is-danger is-small" 
+      onClick={() => handleDeleteMessage(index)}>
+      Eliminar
+    </button>
+  </div>
+))}
+
+              </div>
             </div>
           </div>
         </div>
@@ -110,22 +144,13 @@ const styles = {
     padding: '40px 20px',
     fontFamily: 'Poppins, sans-serif',
   },
-  imageContainer: {
-    position: 'relative',
-    display: 'inline-block',
-    boxShadow: '0 0 15px 5px rgba(72, 199, 142, 0.5)', // Borde luminoso alrededor de la imagen
-    borderRadius: '10px',
+  messages: {
+    maxHeight: '400px',
+    overflowY: 'auto',
+    padding: '10px',
   },
-  image: {
-    display: 'block',
-    borderRadius: '10px',
-    maxWidth: '100%',
-    height: 'auto',
-  },
-  box: {
-    backgroundColor: '#090A0C',
-    padding: '30px',
-    borderRadius: '10px',
+  btnenviar:{
+    backgroundColor: ' #48C78E'
   },
   input: {
     backgroundColor: '#2C2F33',
@@ -136,11 +161,6 @@ const styles = {
     backgroundColor: '#2C2F33',
     color: 'white',
     border: '1px solid #48C78E',
-  },
-  button: {
-    backgroundColor: '#48C78E',
-    borderColor: '#48C78E',
-    color: 'white',
   },
 };
 
