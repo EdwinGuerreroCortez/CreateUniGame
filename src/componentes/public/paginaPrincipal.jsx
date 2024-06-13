4edc7292fcadbb353ac1334c42174cbb32041878
EmmanuelRM17 <emma041117@gmail.com>
2024-06-13 12:50:57 -0600

    add:game

--- src/componentes/public/paginaPrincipal.jsx
+++ src/componentes/public/paginaPrincipal.jsx
@@ -1,5 +1,3 @@
-// src/componentes/public/paginaPrincipal.js
-
 import React from 'react';
 import Slider from 'react-slick';
 import juego2 from '../img/juego2.png';
@@ -9,16 +7,17 @@ import juego5 from '../img/juego5.jpg';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import '../CSS/style.css';
+import 'animate.css/animate.min.css';
 
 const PaginaPrincipal = () => {
   const settings = {
     dots: true,
     infinite: true,
-    speed: 500,
+    speed: 1000, // Velocidad de la transición en milisegundos (ms)
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 2500,
+    autoplaySpeed: 5000, // Tiempo de espera entre transiciones en milisegundos (ms)
     customPaging: (i) => <div className="dot"></div>,
     responsive: [
       {
@@ -46,29 +45,41 @@ const PaginaPrincipal = () => {
         },
       },
     ],
+    beforeChange: (current, next) => {
+      const currentSlide = document.querySelector(`.slick-slide[data-index="${current}"]`);
+      currentSlide.classList.add('animate__animated', 'animate__fadeOut');
+  
+      const nextSlide = document.querySelector(`.slick-slide[data-index="${next}"]`);
+      nextSlide.classList.add('animate__animated', 'animate__fadeIn');
+    },
+    afterChange: (current) => {
+      const currentSlide = document.querySelector(`.slick-slide[data-index="${current}"]`);
+      currentSlide.classList.remove('animate__animated', 'animate__fadeOut');
+      currentSlide.classList.add('animate__animated', 'animate__fadeIn');
+    },
   };
-
+  
   return (
     <section className="section has-background-black-bis">
       <div className="container">
         <div className="columns is-vcentered">
           <div className="column is-half">
-            <div style={{ padding: '40px', justifyContent: 'center' }}>
+            <div className="slider-container">
               <Slider {...settings}>
                 {[juego2, juego3, juego4, juego5].map((imgSrc, index) => (
-                  <div key={index} className="slider-frame" style={{ width: '100%', height: '500px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                    <img src={imgSrc} alt={`Slide ${index + 2}`} style={{ maxWidth: '100%', maxHeight: '100%' }} />
+                  <div key={index} className="slider-slide">
+                    <img src={imgSrc} alt={`Slide ${index + 1}`} className="slider-image" />
                   </div>
                 ))}
               </Slider>
             </div>
           </div>
-          <div className="column is-half" style={{ padding: '20px' }}>
+          <div className="column is-half">
             <div className="card tarjeta-larga has-background-black has-border-white has-shadow">
               <div className="card-content">
                 <div className="content has-text-centered">
-                  <h1 className="title has-text-white">Explora el mundo de la creación de juegos</h1>
-                  <p className="has-text-white">
+                  <h1 className="title has-text-white animate__animated animate__bounceIn">Explora el mundo de la creación de juegos</h1>
+                  <p className="has-text-white animate__animated animate__fadeIn">
                     Nuestra plataforma te ofrece todas las herramientas necesarias para comenzar a desarrollar tus propios videojuegos, abarcando desde conceptos fundamentales hasta técnicas de programación y diseño gráfico.
                   </p>
                   <p className="is-size-4 has-text-white animate__animated animate__bounceInLeft">
@@ -79,11 +90,11 @@ const PaginaPrincipal = () => {
             </div>
           </div>
         </div>
-        <div className="has-text-white has-text-centered" style={{ marginTop: '40px' }}>
+        <div className="has-text-white has-text-centered additional-text animate__animated animate__fadeIn animate__delay-1s" style={{ color: 'green' }}>
           <h2 className="is-size-4">
             Comienza a usar Unity. Da vida a todo lo que puedas imaginar con Unity.
           </h2>
-          <h2 className="is-size-4 animate__animated animate__bounceInLeft color-change">
+          <h2 className="is-size-4 color-change">
             Explora nuestros temas; contamos con videos que explicarán paso a paso el proceso para la creación de un pequeño videojuego.
           </h2>
         </div>
