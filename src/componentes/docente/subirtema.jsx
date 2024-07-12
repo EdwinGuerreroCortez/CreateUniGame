5798120b7fd0e78f4125c7ae7ab0f0b9bcdde48f
EmmanuelRM17 <emma041117@gmail.com>
2024-07-12 10:02:06 -0600

    add:game

--- src/componentes/docente/subirtema.jsx
+++ src/componentes/docente/subirtema.jsx
@@ -169,7 +169,7 @@ const SubirTemaDocente = () => {
   const endIndex = startIndex + pasosPorPagina;
 
   return (
-    <div style={{ minHeight: "100vh", background: '#14161A', marginTop: '20px' }}>
+    <div style={{ minHeight: "100vh", background: '#14161A', paddingTop:'30px'}}>
       {isLoading && (
         <div className="loading-overlay">
           <div className="loading-circle"></div>
